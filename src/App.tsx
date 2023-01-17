8248f5a0ed3f2569a9b57b13468b976ad650312c
Basil <ohuruogubasil@gmail.com>
2023-01-17 11:12:03 +0100

    added:navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
+import Navbar from "@/components/navbar/Navbar"
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="App bg-gray-20">
-      App
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
       
     </div>
   )
